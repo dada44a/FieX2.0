f54ca3bab1f7c6ca7d5c345c22a50a5ea007d54e
Anish Ghimire <pnoob189@gmail.com>
2025-11-08 15:41:35 +0545

    fixed minor issue

--- client/src/components/movie_card.tsx
+++ client/src/components/movie_card.tsx
@@ -1,17 +1,20 @@
 export const MovieCard = () => {
     return (
-        <div className="card lg:card-side md:w-96 bg-base-100 shadow-sm">
+        <div className="card max-w-80 lg:w-96 bg-base-100 shadow-sm">
             <figure>
                 <img
                     loading="lazy"
                     src="https://img.daisyui.com/images/stock/photo-1494232410401-ad00d5433cfa.webp"
                     alt="Album" />
             </figure>
-            <div className="card-body">
-                <h2 className="card-title">New album is released!</h2>
-                <p>Click the button to listen on Spotiwhy app.</p>
-                <div className="card-actions justify-end">
-                    <button className="btn btn-primary">Listen</button>
+            <div className="card-body gap-3">
+                <h2 className="card-title">Coolie (PG)</h2>
+                <div className="flex gap-2">
+                    <div className="badge badge-outline badge-secondary"> <p>Action</p></div>
+                    <div className="badge badge-outline badge-secondary"> <p>Comedy</p></div>
+                </div>
+                <div className="card-actions justify-center">
+                    <button className="btn btn-primary w-full">Enter</button>
                 </div>
             </div>
         </div>
